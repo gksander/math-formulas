e06b7bed3f73d50d08fa30dc94903a8891512202
Grant Sander <gksander93@gmail.com>
2021-03-05 10:55:55 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,16 @@ type HomePageProps = {};
 
 export const HomePage: React.FC<HomePageProps> = () => {
   const Board = useBoard(
-    React.useCallback(({ point, axes, lineSegment, line, circle }) => {
-      axes();
-      const A = point(-3, 3);
-      const B = point(4, 4);
-      const O = point(0, 0);
+    React.useCallback((builder) => {
+      builder.axes();
+      const A = builder.point(-3, 3);
+      const B = builder.point(4, 4);
+      const O = builder.point(0, 0);
 
-      line(A, B);
-      lineSegment(O, A);
-      lineSegment(O, B);
-      circle(A, B);
+      builder.line(A, B);
+      builder.lineSegment(O, A);
+      builder.lineSegment(O, B);
+      builder.circle(A, B);
     }, []),
     {},
   );
